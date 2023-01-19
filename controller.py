d40f47626abd65a3691b5fb09e4b5792506e1f9e
IvanTomaev <ivam.tomaev.01@gmail.com>
2023-01-19 22:01:37 +0300

    Finished work with video stream to tkinter gui:

--- controller.py
+++ controller.py
@@ -1,64 +1,91 @@
 import socket
 import tkinter as tk
-
+import threading
+from PIL import ImageTk, Image, ImageFile
 # import numpy as np
-# import cv2
+import cv2
+import time
 class controller:
     def __init__(self):
         self.port = 2000
-        self.clientIp = ('192.168.31.31', '192.168.43.202','orange.local')
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientIp = ('192.168.31.31', '192.168.43.202', 'orange.local')
         self.pastKey = ''
         flag = True
+        self.mainSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.videoSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.videoSocket.bind(('', 4000))
+        self.videoThread = threading.Thread(target=self.video)
+        self.videoThread.start()
         try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.clientIp[1], self.port))
+            self.mainSocket.connect(('192.168.31.102', self.port))
             print('connection established')
         except:
             print('error')
             flag = False
             pass
         if flag:
-
+            self.window = tk.Tk()
+            self.Label = tk.Label(None, text='Keyboard input')
+            self.window.bind('<KeyPress>', self.keyboard)
+            self.window.bind('<KeyRelease>', self.pause)
+            self.window.bind()
+            self.Label.pack()
+            img = Image.open(r'image.jpg')
+            img = img.resize((640, 480), Image.ANTIALIAS)
+            v = ImageTk.PhotoImage(img)
+            self.panel = tk.Label(image=v)
+            self.panel.pack()
+            img.close()
             self.startSequence()
 
+
+
+
     def pause(self, key):
-        self.socket.send('z'.encode())
+        self.mainSocket.send('z'.encode())
 
     def startSequence(self):
-        window = tk.Tk()
-        Label = tk.Label(None, text = 'Keyboard input')
-        window.bind('<KeyPress>', self.keyboard)
-        window.bind('<KeyRelease>', self.pause)
-        window.bind()
-        Label.pack()
-        window.mainloop()
 
 
+        self.window.mainloop()
 
+    def video(self):
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+        time.sleep(1)
+        while True:
+            self.videoSocket.listen(1)
+            conn, addr = self.videoSocket.accept()
+            print('connected')
+            image = open(r'video.jpg', 'wb')
+            data = 'a'
+            while data:
+                data = conn.recv(2048)
+                image.write(data)
+            image.close()
+            print('received')
+            img = Image.open(r'video.jpg')
+            img = img.resize((640, 480), Image.ANTIALIAS)
+            v = ImageTk.PhotoImage(img)
+            self.panel.config(image=v)
+            self.panel.image = v
+            img.close()
     def keyboard(self, key):
         if key.char != self.pastKey:
-            self.socket.send(key.char.encode())
+            self.mainSocket.send(key.char.encode())
             self.pastKey = key.char
         else:
             pass
 
-    # def video(self):
-    #     data = ''
-    #     while True:
-    #         package = self.sock.recv(1)
-    #         if not package:
-    #             nparr = np.frombytes(data, np.uint8)
-    #             frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    #             cv2.imshow('frame', frame)
-    #             data = ''
-    #             break
-    #         else:
-    #             data += package
 
 
-
-
+class test:
+    def __init__(self):
+        self.socket = socket.socket()
+        self.socket.bind(('', 4000))
+        self.socket.listen(1)
+        self.conn, self.addr = self.socket.accept()
+        while True:
+            self.conn.send(input().encode())
 
 if __name__ == "__main__":
     controller = controller()
--- rosbook/scripts/camera.py
+++ rosbook/scripts/camera.py
@@ -1,26 +1,65 @@
 #! /usr/bin/env python3
+import socket
 import numpy as np
 import cv2
+import multiprocessing as mp
 import rospy
+import time
+import sys
+
+
+class zatichka:
+    def __init__(self):
+        self.socket = socket.socket()
+        self.socket.bind(('', 2000))
+        self.socket.listen(1)
+        self.conn, self.addr = self.socket.accept()
+        
+        while True:
+            data = self.conn.recv(1024)
+            print(data.decode())
+            
+def start():
+    d = zatichka()
 
 
 class videoCapture:
     def __init__(self):
+        process = mp.Process(target = start)
+        process.start()
         rospy.init_node('camera')
         self.capture()
+        
 
     def capture(self):
+        
         video = cv2.VideoCapture(0)
         while True:
+            self.socket = socket.socket()
+            self.socket.connect(('192.168.31.207',4000))
             ret, frame = video.read()
+            data = cv2.imencode('.jpg', frame)[1].tostring()
+            self.socket.sendall(data)
             cv2.imshow('Video', frame)
-
+            time.sleep(0.05)
             if cv2.waitKey(1) == ord('q'):
                 break
+            self.socket.close()
         video.release()
         cv2.destroyAllWindows()
 
 
 
+            
+           
+
+    
+    
+    
+
+
 if __name__ == "__main__":
     camera = videoCapture()
+    
+    
+
